38ad565df01c4a0646fa90f276ea1a98ef8ec6a1
longmin001 <44058133+longmin001@users.noreply.github.com>
2018-10-12 09:15:56 +0800

    Update test.java

--- git/src/git/test.java
+++ git/src/git/test.java
@@ -3,5 +3,6 @@ package git;
 public class test {
     public static void main(String[] args) {
 		System.out.println("sanmao is a dog for longmin");
+	    System.out.println("sanmao is a dog for longmin");
 	}
 }
